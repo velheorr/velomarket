c52a359449b8a5cdc0da2fc8985d3eb41b9b35e9
Iurii <velheorr@gmail.com>
2021-11-29 12:38:11 +0500

    delivery css

--- src/pages/Delivery/Delivery.jsx
+++ src/pages/Delivery/Delivery.jsx
@@ -3,7 +3,6 @@ import s from "./Delivery.module.css";
 import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Divider from "@mui/material/Divider";
-import PaymentIcon from '@mui/icons-material/Payment';
 import Box from "@mui/material/Box";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -13,43 +12,63 @@ import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
 import CreditCardIcon from '@mui/icons-material/CreditCard';
 import MoneyIcon from '@mui/icons-material/Money';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import PaymentsIcon from '@mui/icons-material/Payments';
 
 const Delivery = () => {
 
     return (
-        <div className={`${s.grey_txt}`}>
-            <h2><PaymentIcon fontSize="small"/> Способы оплаты</h2>
+        <div>
+            <ListItem disablePadding className={s.menuHeader}>
+                <ListItemIcon><PaymentsIcon color='primary'/></ListItemIcon>
+                <ListItemText primary="Способы оплаты"/>
+            </ListItem>
             <Divider/>
 
-            <Box sx={{ width: '100%', maxWidth: '100%', paddingLeft:'15px'}}>
-                <List>
-                    <ListItem disablePadding>
-                        <ListItemIcon><QrCodeScannerIcon /></ListItemIcon>
-                        <ListItemText primary="через QR Код" />
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemIcon><CreditCardIcon /></ListItemIcon>
-                        <ListItemText primary="Банковской картой" />
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemIcon><MoneyIcon /></ListItemIcon>
-                        <ListItemText primary="Наличными в магазине" />
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemIcon><AccountBalanceIcon /></ListItemIcon>
-                        <ListItemText primary="Безналичная оплата через банк" />
-                    </ListItem>
-                </List>
-            </Box>
+            <List>
+                <ListItem>
+                    <ListItemIcon><QrCodeScannerIcon /></ListItemIcon>
+                    <ListItemText secondary="через QR Код" />
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon><CreditCardIcon /></ListItemIcon>
+                    <ListItemText secondary="Банковской картой" />
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon><MoneyIcon /></ListItemIcon>
+                    <ListItemText secondary="Наличными в магазине" />
+                </ListItem>
+                <ListItem >
+                    <ListItemIcon><AccountBalanceIcon /></ListItemIcon>
+                    <ListItemText secondary="Безналичная оплата через банк" />
+                </ListItem>
+            </List>
 
-            <h2><ShoppingCartIcon fontSize="small"/> Самовывоз</h2>
+            <ListItem disablePadding className={s.menuHeader}>
+                <ListItemIcon><ShoppingCartIcon color='primary'/></ListItemIcon>
+                <ListItemText primary="Самовывоз"/>
+            </ListItem>
             <Divider/>
-            <p>Заказы можно забрать по адресу: г. Пермь, Борчанинова 62 м-н Веломаркет «Колесо»</p>
-            <h2><DeliveryDiningIcon fontSize="small"/> Курьерская доставка на дом</h2>
+
+            <ListItem>
+                <ListItemText secondary="Заказы можно забрать по адресу: г. Пермь, Борчанинова 62 м-н Веломаркет «Колесо»" />
+            </ListItem>
+
+            <ListItem disablePadding className={s.menuHeader}>
+                <ListItemIcon><DeliveryDiningIcon color='primary'/></ListItemIcon>
+                <ListItemText primary="Курьерская доставка на дом"/>
+            </ListItem>
             <Divider/>
-            <p>Доставка осуществляется с 18:00 до 22:00</p>
-            <p>Стоимость доставки на заказы составляет -  200р</p>
-            <p>Для отдаленных районов - 300р</p>
+
+            <ListItem>
+                <ListItemText secondary="Доставка осуществляется с 18:00 до 22:00" />
+            </ListItem>
+            <ListItem>
+                <ListItemText secondary="Стоимость доставки на заказы составляет -  200р" />
+            </ListItem>
+            <ListItem>
+                <ListItemText secondary="Для отдаленных районов - 300р" />
+            </ListItem>
+
         </div>
 )
 };
