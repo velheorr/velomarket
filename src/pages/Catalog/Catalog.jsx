38b84cfcdbb249fe9319df6f77712ca4750aa294
Iurii <velheorr@gmail.com>
2022-04-07 10:50:57 +0500

    catalog menu + grid

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -42,28 +42,28 @@ const Catalog = () => {
                     {elements}
                 </Grid>
             </Box>
-            <Box sx={{ flexGrow: 1 }} className='wrapper2'>
-                <Grid container spacing={4} rowSpacing={4} className='catalogItems'>
-                    <Grid item md={4}>
-                        <div style={{backgroundImage: `url(${catalogItems[6].img})`}}>1</div>
+            <div className='wrapper2'>
+                <Grid container spacing={4} rowSpacing={4} columns={{ xs: 1, sm: 7, md: 10, lg: 10 }} className='catalogItems'>
+                    <Grid item xs={1} sm={3} md={4} lg={3}>
+                        <div style={{backgroundImage: `url(${catalogItems[6].img})`}}><div>{catalogItems[6].name}</div></div>
                     </Grid>
-                    <Grid item md={4}>
-                        <div style={{backgroundImage: `url(${catalogItems[7].img})`}}>1</div>
+                    <Grid item xs={1} sm={3} md={4} lg={3}>
+                        <div style={{backgroundImage: `url(${catalogItems[7].img})`}}>{catalogItems[7].name}</div>
                     </Grid>
-                    <Grid item md={4}>
-                        <div style={{backgroundImage: `url(${catalogItems[8].img})`}}>1</div>
+                    <Grid item xs={1} sm={3} md={4} lg={3}>
+                        <div style={{backgroundImage: `url(${catalogItems[8].img})`}}>{catalogItems[8].name}</div>
                     </Grid>
-                    <Grid item md={4}>
-                        <div style={{backgroundImage: `url(${catalogItems[9].img})`}}>1</div>
+                    <Grid item xs={1} sm={3} md={4} lg={3}>
+                        <div style={{backgroundImage: `url(${catalogItems[9].img})`}}>{catalogItems[9].name}</div>
                     </Grid>
-                    <Grid item md={4}>
-                        <div style={{backgroundImage: `url(${catalogItems[10].img})`}}>1</div>
+                    <Grid item xs={1} sm={3} md={4} lg={3}>
+                        <div style={{backgroundImage: `url(${catalogItems[10].img})`}}>{catalogItems[10].name}</div>
                     </Grid>
-                    <Grid item md={4}>
-                        <div style={{backgroundImage: `url(${catalogItems[11].img})`}}>1</div>
+                    <Grid item xs={1} sm={3} md={4} lg={3}>
+                        <div style={{backgroundImage: `url(${catalogItems[11].img})`}}>{catalogItems[11].name}</div>
                     </Grid>
                 </Grid>
-            </Box>
+            </div>
         </>
     );
 };
