efc3bae4c7a7cedb6f04a1bdfc5b9597ed4cdc56
Iurii <velheorr@gmail.com>
2022-05-20 09:21:35 +0500

    form style change

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -32,20 +32,20 @@ const Modal = () => {
 			<Paper className='modalContent' elevation={3}>
 				<div className='close' >
 					<div>Форма обратной связи</div>
-					<Button size="small" sx={{color: '#fff'}} onClick={closeModal}>X</Button>
+					<Button size="small" sx={{color: '#000'}} onClick={closeModal}>X</Button>
 				</div>
 				<Divider sx={{mb: '10px'}}/>
 				<div className='modalBody'>
 					<form onSubmit={handleSubmit(onSubmit)}>
-						<TextField {...register("name")}
+						<TextField {...register("name")} className='mb'
 								   size="small" fullWidth label="Имя"/>
 						<TextField {...register("contact", {required: 'Укажите телефон или почту для связи'})}
-								   size="small" fullWidth label="Телефон или почта"
+								   size="small" fullWidth label="Телефон или почта" className='mb'
 								   error={errors.contact && true}
 								   helperText={errors.contact && <span style={{color: 'red'}}>{errors.contact.message}</span>}/>
-						<TextField {...register("text")}
-								   size="small" fullWidth label="Ваш вопрос" multiline rows={4}/>
-						<Button size="small" variant="outlined" type="submit" startIcon={<SendIcon/>}>Отправить</Button>
+						<div className='mb'><TextField {...register("text")} sx={{backgroundColor: '#fff'}}
+										size="small" fullWidth label="Ваш вопрос" multiline rows={4}/></div>
+						<div style={{textAlign: 'center'}}><Button size="small" variant="outlined" type="submit" startIcon={<SendIcon/>}>Отправить</Button></div>
 					</form>
 				</div>
 			</Paper>
