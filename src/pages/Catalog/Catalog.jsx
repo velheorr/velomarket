3b6f33883cb7d738412fd94bb44a08a090d545d1
Iurii <velheorr@gmail.com>
2022-09-30 17:15:34 +0500

    new cat

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -24,10 +24,29 @@ const Catalog = () => {
 
     const catalogElements = renderMainCatalogs(catalogItems);
 
+    const renderMainCatalogs2 = (catalogItems)=>{
+        return  catalogItems.map((item, i) =>
+            <div key={i} item  className='catalog-column'>
+                <Link to={`/catalogs/${item.name}`}>
+                    <div className='catalog-container'>
+                        <div className='catalog-text'>{item.name}</div>
+                        <div style={{backgroundImage: `url(${item.img})`}} className='catalog-item'></div>
+                    </div>
+                </Link>
+            </div>
+        )
+    }
+
+    const catalogElements2 = renderMainCatalogs2(catalogItems);
+
     return (
         <>
             <Slider/>
             <div className='paddingTB blockTitle'><ShoppingCartIcon className='iconAlign' color="primary" /> Каталог товаров</div>
+
+            <div className='wrapper3'>
+               <div className='catalog-row'>{catalogElements2}</div>
+            </div>
 
             <div className='wrapper2'>
                 <Grid container spacing={4} rowSpacing={4} columns={{ xs: 1, sm: 7, md: 10, lg: 10 }} className='catalogItems'>
