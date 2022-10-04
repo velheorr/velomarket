91e3f365e38f0cc5618c6f04d06b6b990fab2a5b
Iurii <velheorr@gmail.com>
2022-10-04 10:21:11 +0500

    header with small menu

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -4,6 +4,7 @@ import logo from './img/logo.png'
 import {Link, NavLink} from "react-router-dom";
 import {showModal} from "../../Modal/ModalSlice";
 import {useDispatch} from "react-redux";
+import SmallMenu from "./smallMenu";
 
 
 const Header = () => {
@@ -15,15 +16,18 @@ const Header = () => {
     return (
         <>
             <div className='navHeader'>
-                <span><Link to="/"><img src={logo} alt={'velomarketkoleso.ru'}/></Link></span>
-                <ul>
-                    <li><NavLink to="/" exact className={isActive => (isActive ? 'active' : '')}>Каталог</NavLink></li>
-                    <li><NavLink to="/repair" className={isActive => (isActive ? 'active' : '')}>Сервис и Ремонт</NavLink></li>
-                    <li><NavLink to="/delivery" className={isActive => (isActive ? 'active' : '')}>Оплата и Доставка</NavLink></li>
-                    <li><NavLink to="/contacts" className={isActive => (isActive ? 'active' : '')}>Контакты</NavLink></li>
-                    <li onClick={openModal}><a href="#/">Обратная связь</a></li>
-                </ul>
-                <span className='headerInfo'>Веломаркет Колесо</span>
+                <div className='navHeader-img'><Link to="/"><img src={logo} alt={'velomarketkoleso.ru'}/></Link></div>
+                <div className='navHeader-title'>Веломаркет Колесо</div>
+                <div className='navHeader-menu'>
+                    <ul>
+                        <li><NavLink to="/" exact className={isActive => (isActive ? 'active' : '')}>Каталог</NavLink></li>
+                        <li><NavLink to="/repair" className={isActive => (isActive ? 'active' : '')}>Сервис и Ремонт</NavLink></li>
+                        <li><NavLink to="/delivery" className={isActive => (isActive ? 'active' : '')}>Оплата и Доставка</NavLink></li>
+                        <li><NavLink to="/contacts" className={isActive => (isActive ? 'active' : '')}>Контакты</NavLink></li>
+                        <li onClick={openModal}><a href="#/">Обратная связь</a></li>
+                    </ul>
+                    <SmallMenu css={'navHeaderMiniMenu'}/>
+                </div>
             </div>
         </>
     );
---
+++ src/pages/Header/smallMenu.jsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {useState} from "react";
+
+const SmallMenu =({css})=> {
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	return (
+		<>
+			<Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }} className={css}>
+					<IconButton
+						onClick={handleClick}
+						sx={{ ml: 2 }}
+						aria-controls={open ? 'account-menu' : undefined}
+						aria-haspopup="true"
+						aria-expanded={open ? 'true' : undefined}
+					>
+						<MenuIcon/>
+					</IconButton>
+			</Box>
+			<Menu
+				anchorEl={anchorEl}
+				id="account-menu"
+				open={open}
+				onClose={handleClose}
+				onClick={handleClose}
+				PaperProps={{
+					elevation: 0,
+					sx: {
+						overflow: 'visible',
+						filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+						mt: 1.5,
+						'& .MuiAvatar-root': {
+							width: 32,
+							height: 32,
+							ml: -0.5,
+							mr: 1,
+						},
+						'&:before': {
+							content: '""',
+							display: 'block',
+							position: 'absolute',
+							top: 0,
+							right: 14,
+							width: 10,
+							height: 10,
+							bgcolor: 'background.paper',
+							transform: 'translateY(-50%) rotate(45deg)',
+							zIndex: 0,
+						},
+					},
+				}}
+				transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+				anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+			>
+
+				<MenuItem>Каталог</MenuItem>
+				<MenuItem>Сервис и ремонт</MenuItem>
+				<MenuItem>Оплата и доставка</MenuItem>
+				<MenuItem>Контакты</MenuItem>
+				<MenuItem>Обратная связь</MenuItem>
+			</Menu>
+		</>
+	);
+}
+
+export default SmallMenu;
